d63455118a987e727e23de63ea762cb235eb83ae
Ty Mick <ty@tymick.me>
2020-10-27 15:43:09 -0500

    Finish dev function

--- dev.zsh
+++ dev.zsh
@@ -1,3 +1,41 @@
-# function dev {
-#     if [[ !]]
-# }
+autoload throw
+
+# Open a local repo in VSCodium if it exists on my machine. If it doesn't,
+# search for the repo on GitHub, fork it if it isn't mine, clone it onto my
+# machine, and then open it in VSCodium.
+function dev() {
+    if [[ -d ~/dev/$1 ]]; then
+        code ~/dev/$1
+    else
+        if [[ -d ~/dev/*/$1 ]]; then
+            code ~/dev/*/$1
+        else
+            if [[ $1 =~ "^([^/]*)/([^/].*)$" ]]; then
+                mkdir -p ~/dev/$match[1]
+                if [[ $match[1] == "tywmick" ]]; then
+                    if gh repo clone $1 -- ~/dev/$1; then
+                        code ~/dev/$1
+                    else
+                        throw
+                    fi
+                else
+                    if gh repo fork $1 --clone=false; then
+                        if gh repo clone tywmick/$match[2] -- ~/dev/$1 --depth 1; then
+                            code ~/dev/$1
+                        else
+                            throw
+                        fi
+                    else
+                        throw
+                    fi
+                fi
+            else
+                if gh repo clone $1 -- ~/dev/tywmick/$1; then
+                    code ~/dev/tywmick/$1
+                else
+                    throw
+                fi
+            fi
+        fi
+    fi
+}
