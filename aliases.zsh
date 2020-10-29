9e8573a7acf2eb137d27e33463632f605f653153
Ty Mick <ty@tymick.me>
2020-10-29 16:36:54 -0500

    Switch omzc to function that'll automatically apply changes

--- aliases.zsh
+++ aliases.zsh
@@ -3,5 +3,3 @@ alias py=python3
 alias pip=pip3
 
 alias deploy="git push; npm run deploy"
-
-alias omzc="code ~/.oh-my-zsh/custom"
---
+++ omzc.zsh
@@ -0,0 +1,4 @@
+function omzc() {
+    code ~/.oh-my-zsh/custom --wait
+    exec zsh
+}
