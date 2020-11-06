04f1d79274dc2fe2e7be890b81d8f669bf3eb274
Ty Mick <ty@tymick.me>
2020-11-06 15:09:39 -0600

    Reorganize

--- homebrew.zsh
+++
@@ -1,2 +0,0 @@
-# Homebrew environment
-export HOMEBREW_NO_INSTALL_CLEANUP=1
--- variables.zsh
+++ variables.zsh
@@ -1,3 +1,7 @@
 export EDITOR="code --wait"
 
-ZSH_ALIAS_FINDER_AUTOMATIC=true
+# Homebrew
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+# Oh My Zsh alias-finder plugin
+export ZSH_ALIAS_FINDER_AUTOMATIC=true
