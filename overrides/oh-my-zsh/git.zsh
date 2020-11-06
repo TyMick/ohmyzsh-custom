ccbc5141e57b9327d45e0f60d07285dd970f0b90
Ty Mick <ty@tymick.me>
2020-11-06 15:20:15 -0600

    Overwrite omz "gca" aliases

--- overrides/oh-my-zsh/git.zsh
+++ overrides/oh-my-zsh/git.zsh
@@ -1,5 +1,10 @@
-unalias grs
+alias gca='git add --all && git commit -v'
+alias 'gca!'='git add --all && git commit -v --amend'
+alias gcam='git add --all && git commit -m'
+alias 'gcan!'='git add --all && git commit -v --no-edit --amend'
+alias 'gcans!'='git add --all && git commit -v -s --no-edit --amend'
 
+unalias grs
 function grs() {
     git reset --soft "HEAD~$1"
 }
