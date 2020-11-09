d7c875f3548cea572328a11e7ba9555566fa8884
Ty Mick <ty@tymick.me>
2020-11-09 14:43:23 -0600

    Add new git commit amend shortcuts

--- overrides.zsh
+++ overrides.zsh
@@ -1,11 +1,13 @@
-#************************#
+#========================#
 # Oh My Zsh - git plugin #
-#************************#
+#========================#
 alias gca='git add --all && git commit -v'
 alias 'gca!'='git add --all && git commit -v --amend'
 alias gcam='git add --all && git commit -m'
+alias 'gcam!'='git add --all && git commit --amend -m' # Addition
 alias 'gcan!'='git add --all && git commit -v --no-edit --amend'
 alias 'gcans!'='git add --all && git commit -v -s --no-edit --amend'
+alias 'gcmsg!'='git commit --amend -m' # Addition
 
 unalias grs
 function grs() {
