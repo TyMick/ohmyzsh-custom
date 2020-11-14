e6946af89fd05471d288c6015866ec40f4601358
Ty Mick <ty@tymick.me>
2020-11-14 11:24:02 -0600

    Link to zsh-syntax-highlighting for future reference

--- z-last.zsh
+++ z-last.zsh
@@ -1,5 +1,6 @@
-#
-# Commands that need to be executed at the end of .zshrc
-#
+#==============================================================================#
+# Commands that need to be executed at the end of .zshrc                       #
+#==============================================================================#
 
+# zsh-syntax-highlighting: https://github.com/zsh-users/zsh-syntax-highlighting
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
