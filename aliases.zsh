260817aef20e4196b7051f9b482fcbb83846dd3f
Ty Mick <ty@tymick.me>
2020-11-14 11:20:11 -0600

    Add an update-all alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,9 @@
-alias aliases="alias-finder -l"
+alias aliases='alias-finder -l'
 
-alias deploy="git push; npm run deploy"
+alias deploy='git push; npm run deploy'
 
-alias pip="pip3"
-alias python="python3"
-alias py="python3"
+alias pip='pip3'
+alias python='python3'
+alias py='python3'
+
+alias update-all='brew upgrade; npm -g update; conda update --all'
