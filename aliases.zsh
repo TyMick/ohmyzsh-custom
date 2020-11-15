c6d7498c45e97442ba1c625584123f8371ef1edd
Ty Mick <ty@tymick.me>
2020-11-15 09:41:13 -0600

    separate custom git aliases from omz defaults

--- aliases.zsh
+++ aliases.zsh
@@ -2,6 +2,14 @@ alias aliases='alias-finder -l'
 
 alias deploy='git push; npm run deploy'
 
+alias gcaa='git add --all && git commit -v'
+alias 'gcaa!'='git add --all && git commit -v --amend'
+alias gcaam='git add --all && git commit -m'
+alias 'gcaam!'='git add --all && git commit --amend -m'
+alias 'gcaan!'='git add --all && git commit -v --no-edit --amend'
+alias 'gcaans!'='git add --all && git commit -v -s --no-edit --amend'
+alias 'gcmsg!'='git commit --amend -m'
+
 alias pip='pip3'
 alias python='python3'
 alias py='python3'
