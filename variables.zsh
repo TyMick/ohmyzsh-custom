4cc8c8b80176de573aa297d8cdeda4d1b3e40450
Ty Mick <ty@tymick.me>
2021-08-12 07:32:35 -0700

    Disable Next.js telemetry

--- variables.zsh
+++ variables.zsh
@@ -1,16 +1,15 @@
 export EDITOR="code --new-window --wait"
 
-# Gatsby
-export GATSBY_TELEMETRY_DISABLED=1
-
 # Homebrew
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
 # n
 export N_PRESERVE_NPM=1
 
-# .NET SDK
-export DOTNET_CLI_TELEMETRY_OPTOUT=true
-
 # Oh My Zsh alias-finder plugin
 export ZSH_ALIAS_FINDER_AUTOMATIC=true
+
+# disabling telemetry
+export DOTNET_CLI_TELEMETRY_OPTOUT=true
+export GATSBY_TELEMETRY_DISABLED=1
+export NEXT_TELEMETRY_DISABLED=1
