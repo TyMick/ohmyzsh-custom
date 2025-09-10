b73936362f56e08ea95aedc5a563b9f6b6271e6f
Ty Mick <5317080+TyMick@users.noreply.github.com>
2025-09-10 13:11:17 -0700

    Update zsh-syntax-highlighting path to Homebrew location

--- zz-last.zsh
+++ zz-last.zsh
@@ -3,4 +3,4 @@
 #==============================================================================#
 
 # zsh-syntax-highlighting: https://github.com/zsh-users/zsh-syntax-highlighting
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
