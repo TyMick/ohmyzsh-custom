6740fe915120f92bee66b5c50ca2cb06c7b6d6e4
Ty Mick <5317080+TyMick@users.noreply.github.com>
2026-03-23 12:04:07 -0700

    Add git aliases for simplified pull, commit, and stash operations

--- aliases.zsh
+++ aliases.zsh
@@ -5,22 +5,6 @@ alias enable-shadow='defaults write com.apple.screencapture disable-shadow -bool
 
 alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox'
 
-alias glom='git pull origin master --rebase'
-alias glomm='git pull origin master --rebase=false'
-
-alias gcaa='git add --all && git commit -v'
-alias 'gcaa!'='git add --all && git commit -v --amend'
-alias gcaam='git add --all && git commit -m'
-alias 'gcaam!'='git add --all && git commit --amend -m'
-alias 'gcaan!'='git add --all && git commit -v --no-edit --amend'
-alias 'gcaans!'='git add --all && git commit -v -s --no-edit --amend'
-alias 'gcmsg!'='git commit --amend -m'
-alias gstaa='git stash --all --include-untracked'
-
-# double-stash
-alias gdsta='git stash push --keep-index && git stash push && git stash pop 1'
-alias gdstaa='git stash --all --include-untracked --keep-index && git stash --all --include-untracked && git stash pop 1'
-
 alias mmv='noglob zmv -W'
 
 alias pip='pip3'
--- git.zsh
+++ git.zsh
@@ -1,3 +1,25 @@
+alias glomr='git pull origin $(git_main_branch) --rebase'
+alias glomm='git pull origin $(git_main_branch) --rebase=false'
+
+alias gcaa='git add --all && git commit -v'
+alias 'gcaa!'='git add --all && git commit -v --amend'
+alias gcaam='git add --all && git commit -m'
+alias 'gcaam!'='git add --all && git commit --amend -m'
+alias 'gcaan!'='git add --all && git commit -v --no-edit --amend'
+alias 'gcaans!'='git add --all && git commit -v -s --no-edit --amend'
+alias 'gcmsg!'='git commit --amend -m'
+alias gstaa='git stash --all --include-untracked'
+
+# git switch (shorter alternatives to gsw* from the git plugin)
+alias gs='git switch'
+alias gsc='git switch --create'
+alias gsd='git switch $(git_develop_branch)'
+alias gsm='git switch $(git_main_branch)'
+
+# double-stash
+alias gdsta='git stash push --keep-index && git stash push && git stash pop 1'
+alias gdstaa='git stash --all --include-untracked --keep-index && git stash --all --include-untracked && git stash pop 1'
+
 # Double stash all with message
 function gdstaam() {
   git stash --all --include-untracked --keep-index
