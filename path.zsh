f2928404bb476ca20850bb75041e4b9922f0ba0f
Ty Mick <5317080+TyMick@users.noreply.github.com>
2026-03-23 11:54:49 -0700

    Refactor PATH management in path.zsh and add fnm.zsh for Node version management

--- path.zsh
+++ path.zsh
@@ -1,2 +1,20 @@
-export PATH="/usr/local/sbin:$PATH"
-export PATH="$PATH:/Users/ty.mick/.dotnet/tools"
+export PNPM_HOME="/Users/ty.mick/Library/pnpm"
+
+() {
+  local paths_to_prepend=(
+    "$PNPM_HOME"
+  )
+
+  local paths_to_append=(
+    "/Users/ty.mick/.dotnet/tools"
+  )
+
+  local p
+  for p in ${(Oa)paths_to_prepend}; do
+    [[ ":$PATH:" != *":$p:"* ]] && export PATH="$p:$PATH"
+  done
+
+  for p in $paths_to_append; do
+    [[ ":$PATH:" != *":$p:"* ]] && export PATH="$PATH:$p"
+  done
+}
